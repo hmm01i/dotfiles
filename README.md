c272ad3f1737eb8d308f6dc79ad6a27da8e2373a
Jacob Sohl <jacobsohl@gmail.com>
2015-08-13 21:43:31 -0700

    setup script and updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # customizations
+
+Some of this stuff should probably be done by puppet.
+But we can rely on puppet being present. So bash is probably just best.
+But we can have a puppet module...
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+echo "Installing bash git prompt"
+cd ~
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+
+echo "Installing puppet git hooks"
+cd ~
+git clone https://github.com/drwahl/puppet-git-hooks.git .puppet-git-hooks
+
