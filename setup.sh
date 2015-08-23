fc289751674727c75210a8491cd1b0fa200f6e98
Jacob Sohl <jacobsohl@gmail.com>
2015-08-23 00:41:24 -0700

    conditionals

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #/bin/bash
 
-echo "Installing bash git prompt"
-cd ~
-git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+read -p "Install bash git prompt (Y/n)" prompt
+echo $prompt
+if [ "$prompt" != "n" ] && [ "$prompt" != "N" ]
+then
+  echo "Installing bash git prompt"
+  cd ~
+  git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+fi
 
-echo "Installing puppet git hooks"
-cd ~
-git clone https://github.com/drwahl/puppet-git-hooks.git .puppet-git-hooks
-
+read -p "Install puppet git hooks (Y/n)" prompt
+if [ "$prompt" != "n" ] && [ "$prompt" != "N" ]
+then
+  echo "Installing puppet git hooks"
+  cd ~
+  git clone https://github.com/drwahl/puppet-git-hooks.git .puppet-git-hooks
+fi
