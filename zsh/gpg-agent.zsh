c1e805d50b21d01352041f704e80492aa4931270
Jacob Sohl <jacobsohl@gmail.com>
2016-11-23 17:06:37 -0800

    zsh dotfiles and oh-my-zsh

--- zsh/gpg-agent.zsh
+++ zsh/gpg-agent.zsh
@@ -3,7 +3,7 @@
 ps -C gpg-agent > /dev/null
 RTN=$(echo $?)
 if [[ $RTN -ne 0 ]]; then
-  eval $(gpg-agent --debug 9 --daemon --enable-ssh-support --write-env-file "${HOME}/.gpg-agent-info" --log-file '/home/jsohl/.gnupg/log/gpg-agent.log')
+  eval $(gpg-agent --debug 9 --daemon --enable-ssh-support "${HOME}/.gpg-agent-info" --log-file '/home/jsohl/.gnupg/log/gpg-agent.log')
 fi
 
 if [ -f "${HOME}/.gpg-agent-info" ]; then
