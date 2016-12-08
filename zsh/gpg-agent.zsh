9dbc33f4d94db4241759eee4ce009831f511b9cc
Jacob Sohl <jacobsohl@gmail.com>
2016-12-07 21:15:34 -0800

    check if gpg-agent exists before trying to load it

--- zsh/gpg-agent.zsh
+++ zsh/gpg-agent.zsh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-ps -C gpg-agent > /dev/null
-RTN=$(echo $?)
-if [[ $RTN -ne 0 ]]; then
-  eval $(gpg-agent --debug-level advanced --daemon --enable-ssh-support --write-env-file "${HOME}/.gpg-agent-info" --log-file "${HOME}/.gnupg/log/gpg-agent.log")
+if [ "$(type gpg-agent; echo $?)" = "0" ]; then
+    ps -C gpg-agent > /dev/null
+    RTN=$(echo $?)
+    if [[ $RTN -ne 0 ]]; then
+      eval $(gpg-agent --debug-level advanced --daemon --enable-ssh-support --write-env-file "${HOME}/.gpg-agent-info" --log-file "${HOME}/.gnupg/log/gpg-agent.log")
+    fi
+
+    if [ -f "${HOME}/.gpg-agent-info" ]; then
+      . "${HOME}/.gpg-agent-info"
+      export GPG_AGENT_INFO
+      export SSH_AUTH_SOCK
+      export SSH_AGENT_PID
+      gpg-connect-agent updatestartuptty /bye > /dev/null
+    fi
+
+    GPG_TTY=$(tty)
+    export GPG_TTY
+elif [ "$(type ssh-pageant; echo $?)" = 0 ];then
+    eval $(/usr/bin/ssh-pageant -r -a "/tmp/.ssh-pageant-$USERNAME")
 fi
-
-if [ -f "${HOME}/.gpg-agent-info" ]; then
-  . "${HOME}/.gpg-agent-info"
-  export GPG_AGENT_INFO
-  export SSH_AUTH_SOCK
-  export SSH_AGENT_PID
-  gpg-connect-agent updatestartuptty /bye > /dev/null
-fi
-
-GPG_TTY=$(tty)
-export GPG_TTY
-
