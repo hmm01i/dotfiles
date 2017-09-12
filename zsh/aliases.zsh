9e78c2fe451ea66337103b0b94fcaa7ac0c58a09
Jacob Sohl <jacob.sohl1@t-mobile.com>
2017-09-12 18:04:56 -0400

    default --diff to ansible alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 type vim 2>&1 > /dev/null && alias vi="vim"
-alias apl="ansible-playbook"
+alias apl="ansible-playbook --diff"
 alias agl="ansible-galaxy"
 alias aglreq="ansible-galaxy install -r requirements.yml"
