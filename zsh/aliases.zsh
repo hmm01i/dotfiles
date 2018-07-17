bdc388e147a95f66c06803e00da300c40e87370f
Jacob Sohl <jacobsohl@gmail.com>
2018-07-16 22:12:15 -0700

    minor configs for tmux and vim + alias for minikube

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias apl="ansible-playbook --diff"
 alias aplprod="ANSIBLE_CONFIG=~/.ansible/prod.cfg apl"
 alias agl="ansible-galaxy"
 alias aglreq="agl install -r requirements.yml"
+
+#for using minikube
+alias mkdkr="eval $(minikube docker-env)"
