fea2ce52a0b514d16ea277f4304e1401fc545a9d
Jacob Sohl <jsohl@starbucks.com>
2018-10-05 11:44:28 -0700

    remove debugging echos

--- zsh/ssh-agent.zsh
+++ zsh/ssh-agent.zsh
@@ -1,15 +1,13 @@
 #!/bin/bash -x
-echo "ssh-agent.zsh running"
+#echo "ssh-agent.zsh running"
 if [ "$USE_SSH_AGENT" = "true" ]; then
   echo "USE_SSH_AGENT = true"
   if [[ -f ~/.ssh/ssh-agent.env ]]; then
-    echo "ssh-agent.env is a file. sourcing it."
+   echo "ssh-agent.env is a file. sourcing it."
     . ~/.ssh/ssh-agent.env > /dev/null
   fi
-#  checking for ssh-agent pid seems to not be working
-  echo "checking for ssh-agent process. what if the pid exists but it's not ssh-agent."
-  echo "ps:"
-  echo "$(ps -p $SSH_AGENT_PID)"
+#  echo "checking for ssh-agent process. what if the pid exists but it's not ssh-agent."
+#  echo "$(ps -p $SSH_AGENT_PID)"
   if ! ps -p $SSH_AGENT_PID > /dev/null; then
     echo "ssh-agent seems to be dead... starting new agent"
     ssh-agent -s > ~/.ssh/ssh-agent.env
