9c2deb8e7e57cad15f38bf3bcd7d4370d0615357
Jacob Sohl <jsohl@starbucks.com>
2019-03-04 10:21:54 -0800

    quiet the script

--- zsh/azure.zsh
+++ zsh/azure.zsh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # check if az is installed
   # if so, load bash completion for az-cli
-if type az;
+if type az 2>&1 > /dev/null;
 then
   autoload -U +X bashcompinit && bashcompinit
     source /usr/local/etc/bash_completion.d/az
