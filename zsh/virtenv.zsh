0886908b45411f8dd4a5313e92f1b608ce8ef820
Jacob Sohl <jsohl@starbucks.com>
2023-06-01 14:34:53 -0700

    add openai personas

--- zsh/virtenv.zsh
+++ zsh/virtenv.zsh
@@ -1,6 +1,7 @@
 if type virtualenvwrapper.sh 2>&1 > /dev/null;
 then
   WRAPPER_CMD=$(which virtualenvwrapper.sh)
+#  export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
   export WORKON_HOME=$HOME/.virtualenvs
   export PROJECT_HOME=$HOME/workspace
   source $WRAPPER_CMD
